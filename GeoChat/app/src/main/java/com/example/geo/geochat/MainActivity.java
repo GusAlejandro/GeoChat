fd180c7ec6ff16f63dc3f0c87502bab93ed7a388
schoudhury <subho.choudhury@gmail.com>
2017-04-23 13:41:26 -0700

    added bottom navigation and set the init to photo album activity, but no new activities created on click, just textView set text change

--- GeoChat/app/src/main/java/com/example/geo/geochat/MainActivity.java
+++ GeoChat/app/src/main/java/com/example/geo/geochat/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.geo.geochat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+//    TODO: fix the navigation labels
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_camera);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_album);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_uploads);
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_dashboard);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
+
